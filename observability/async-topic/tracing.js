2bae05b1416bf2abc016defa47720c193bbd350c
up1 <somkiat.p@gmail.com>
2024-03-04 08:05:05 +0700

    Change to otel-collector

--- observability/async-topic/tracing.js
+++ observability/async-topic/tracing.js
@@ -16,21 +16,21 @@ const sdk = new opentelemetry.NodeSDK({
     traceExporter: new OTLPTraceExporter({
         // optional - default url is http://localhost:4318/v1/traces
         //url: 'http://localhost:9193/v1/traces',
-        url: process.env.OTEL_COLLECTOR_SERVER || 'http://localhost:4318/v1/traces', // Send to Jaeger (otel collector)
+        url: process.env.OTEL_COLLECTOR_SERVER || 'http://localhost:14318/v1/traces', // Send to Jaeger (otel collector)
         // optional - collection of custom headers to be sent with each request, empty by default
         headers: {},
     }),
-    metricReader: new PeriodicExportingMetricReader({
-        exporter: new ConsoleMetricExporter(),
-        exportIntervalMillis: 1000,
-    }),
     // metricReader: new PeriodicExportingMetricReader({
-    //     exporter: new OTLPMetricExporter({
-    //         //url: '<your-otlp-endpoint>/v1/metrics', // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
-    //         headers: {}, // an optional object containing custom headers to be sent with each request
-    //         concurrencyLimit: 1, // an optional limit on pending requests
-    //     }),
+    //     exporter: new ConsoleMetricExporter(),
+    //     exportIntervalMillis: 1000,
     // }),
+    metricReader: new PeriodicExportingMetricReader({
+        exporter: new OTLPMetricExporter({
+            url: 'http://localhost:14318/v1/metrics', // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
+            headers: {}, // an optional object containing custom headers to be sent with each request
+            concurrencyLimit: 1, // an optional limit on pending requests
+        }),
+    }),
     instrumentations: [
         getNodeAutoInstrumentations(),
         new AmqplibInstrumentation(),
