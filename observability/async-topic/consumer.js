893d7af9e1490ed4377e93d04c84112014457a06
up1 <somkiat.p@gmail.com>
2024-06-26 13:13:14 +0700

    custom trace

--- observability/async-topic/consumer.js
+++ observability/async-topic/consumer.js
@@ -11,5 +11,16 @@ const rabbitMqListenToMessages = async (callback) => {
     await channel.bindQueue(q.queue, exchange, '');
     await channel.consume(q.queue, (message) => callback(message.content.toString()), { noAck: true })
 }
-rabbitMqListenToMessages((message) => console.log(`Consumer received message: ${message}`))
+rabbitMqListenToMessages(
+    (message) => 
+        {
+            console.log(`Consumer received message: ${message}`)
+            // Create span from request
+            const opentelemetry = require('@opentelemetry/api');
+            const tracer = opentelemetry.trace.getTracer('consumer-tracer');
+            const span = tracer.startSpan('consumer-received-message');
+            span.setAttribute('message', message);
+            span.end();
+        }
+)
 console.log(`${process.env.SERVICE} Running`)
